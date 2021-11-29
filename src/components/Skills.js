8125977d3218184525dedf60f662205eaaf5cfd9
Omar D. Ramirez <omardramirez2002@gmail.com>
2021-11-29 08:59:07 -0600

    This commit adds react and graphQl to my skills portion.

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -11,6 +11,8 @@ export default function Skills(props){
                     <p class="skillP">CSS</p>
                     <p class="skillP">Javascript</p>
                     <p class="skillP">SQL</p>
+                    <p class="skillP">GraphQl</p>
+                    <p class="skillP">React</p>
                     <p class="skillP">Express</p>
                     <p class="skillP">MongoDb</p>
                     <p class="skillP">Express Handlebars</p>
