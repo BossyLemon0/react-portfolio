32531eeaee7f2abb3cb222ef8e4ce3ef59217cc0
Omar D. Ramirez <omardramirez2002@gmail.com>
2021-11-29 08:49:52 -0600

    This commit adds some of the text for my skills slide.

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -4,7 +4,19 @@ import React from "react";
 export default function Skills(props){
     return (
     <div className = 'slides'>
-        <div>Skills</div>
+        <div class="second">
+            <div class="texty">
+                <header class="skillsHeader"> My skills</header>
+                    <p class="skillP">HTML</p>
+                    <p class="skillP">CSS</p>
+                    <p class="skillP">Javascript</p>
+                    <p class="skillP">SQL</p>
+                    <p class="skillP">Express</p>
+                    <p class="skillP">MongoDb</p>
+                    <p class="skillP">Express Handlebars</p>
+                    <p class="skillP">Luxon</p>
+            </div>
+        </div>
     </div>
     );
 }
